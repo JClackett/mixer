2ef4e38b7f817e896e776d4f94e3fac79747cee1
Jack Clackett <jack@noquarter.co>
2025-02-25 17:55:51 +0100

    og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 export const metadata: Metadata = {
   title: "Mixer - J3-C7",
   description: "Soundscape mixer to relax and zone out",
+  openGraph: {
+    images: [{ url: "/cover.png" }],
+  },
 }
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
