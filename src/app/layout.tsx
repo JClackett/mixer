9ba031e5138be0766dfe95c4d03fc743a9fd5f14
Jack Clackett <jack@noquarter.co>
2025-03-04 23:06:26 +0100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
   title: "Mixer - J3-C7",
   description: "Soundscape mixer to relax and zone out",
   openGraph: {
-    images: [{ url: "/cover.png" }],
+    images: [{ url: "/cover.png?v2" }],
   },
 }
 
